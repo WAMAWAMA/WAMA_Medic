85a44340e79467e0d0fc304cf3f9ec738fd2b7cf
WAMAWAMA <wmy19970215@gmail.com>
2020-10-01 21:06:13 +0800

    big upup

--- proj/wama/data_augmentation.py
+++ proj/wama/data_augmentation.py
@@ -13,6 +13,15 @@ import imgaug as ia
 from imgaug import augmenters as iaa
 
 
+
+
+"""扩增需要注意的"""
+# batch_dict，字典，key为data和seg
+
+
+
+
+
 """2D 的扩增"""
 # 2D 的扩增 --------------------------------------------------------------
 
@@ -29,7 +38,9 @@ def BG_augmenter(batch_dict):
 
 # PIL的（简称PIL）
 # 需要手动考虑是否有seg, 且需要分别实现seg和img两个的变换（因为插值等问题）
+# 注意维度，PIL的channel轴在最后一维，即batch的shape是（bz，w，h，c），所以需要调整batch_dict里面数据的dim order才能处#理
 # 扩增概率需要手动实现
+# 需要抓换为pil类型的，才能继续使用PIL处理
 PIL_transforms_seq_img = []
 PIL_transforms_seq_mask = []
 PIL_transforms_seq_img.append()
@@ -37,6 +48,46 @@ PIL_transforms_seq_mask.append()
 PIL_transforms_img = T.Compose(PIL_transforms_seq_img)
 PIL_transforms_mask = T.Compose(PIL_transforms_seq_mask)
 def PIL_augmenter(batch_dict):
+    # 获得图片
+    tmp_patch_data = batch_dict['data']  # ndarray with shape （bz，c，w，h）
+    if 'seg' in batch_dict.keys():
+        tmp_patch_mask = batch_dict['seg']  # if not None, ndarray with shape （bz，c，w，h）
+    else:
+        tmp_patch_mask = None
+
+
+    # 调整维度，将（bz，c，w，h）调整为AG可以处理的（bz，w，h，c）
+    tmp_patch_data = np.transpose(tmp_patch_data, (0, 2, 3, 1))
+    if tmp_patch_mask is not None:
+        tmp_patch_mask = np.transpose(tmp_patch_mask, (0, 2, 3, 1))
+
+    # PIL不能对batch处理，故需要写个循环来处理
+    for i in tmp_patch_data.shape[0]:
+        # 取出单个sample
+        _img = tmp_patch_data[i,:,:,:]
+        if tmp_patch_mask is not None:
+            _mask = tmp_patch_mask[i,:,:,:]
+
+        #  转换为PIL独有的格式
+        _img = Image.fromarray(_img)
+        if tmp_patch_mask is not None:
+            _mask = Image.fromarray(_mask)
+
+        # 淦
+        _img = PIL_transforms_img(_img)
+        if tmp_patch_mask is not None:
+            _mask = PIL_transforms_mask(_mask)
+
+        # 转换回numpy，储存到原tmp_patch_data
+
+
+    # 调整dim顺序
+
+
+
+
+    # 返回
+
     raise NotImplementedError  # todo
 
 
@@ -44,7 +95,8 @@ def PIL_augmenter(batch_dict):
 
 
 # imgaug的(简称AG）
-# 需要手动考虑是否有seg， 注意维度，AG的channel轴再最后一维，即batch的shape是（bz，w，h，c）
+# 需要手动考虑是否有seg，
+# 注意维度，AG的channel轴在最后一维，即batch的shape是（bz，w，h，c），所以需要调整batch_dict里面数据的dim order才能处理
 # 扩增概率已经内部实现，设置参数即可
 AG_transforms_seq = ([])
 AG_transforms_seq.append()
